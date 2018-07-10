41ba54a8fc55329fd53c4b9b4948ce775cbf3a5b
Amr ELghadban <karmaadam.team@gmail.com>
2018-07-10 14:03:47 +0200

    adding WebService test performance

--- NYTimes/NYTimesTests/APIRequestHandlerTest.swift
+++ NYTimes/NYTimesTests/APIRequestHandlerTest.swift
@@ -21,26 +21,26 @@ class APIRequestHandlerTest: XCTestCase {
 
     /// This is an example of a performance test case for calling MostViewed API
     func testMostViewedAPICall() {
-//        let expectation = self.expectation(description: "Response Received")
-//        // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//            guard let urlRoute = URLFactory.getURL(.mostviewed, section: "all-sections", timePeriod: "7").url else {
-//                return
-//            }
-//
-//            guard let url = APIRequestHandler.share.createRequest(url: urlRoute, forceUpdate: false) else {
-//                return
-//            }
-//
-//            APIRequestHandler.share.InvokeURLRequest(request: url) { response in
-//                XCTAssertNotNil(response)
-//                expectation.fulfill()
-//            }
-//        }
+        let expectation = self.expectation(description: "Response Received")
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+            guard let urlRoute = URLFactory.getURL(.mostviewed, section: "all-sections", timePeriod: "7").url else {
+                return
+            }
 
-//        self.waitForExpectations(timeout: 30) { error in
-//            XCTAssertNil(error)
-//        }
+            guard let url = APIRequestHandler.share.createRequest(url: urlRoute, forceUpdate: false) else {
+                return
+            }
+
+            APIRequestHandler.share.InvokeURLRequest(request: url) { response in
+                XCTAssertNotNil(response)
+                expectation.fulfill()
+            }
+        }
+
+        self.waitForExpectations(timeout: 30) { error in
+            XCTAssertNil(error)
+        }
     }
 }
